2708b03f7a76ff086e6ee7ad579971463270a602
p1p1ng <you@example.com>
2026-01-19 00:35:30 +0700

    tsup

--- my-ui-library/tsup.config.js
+++ my-ui-library/tsup.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.js"],
   format: ["esm", "cjs"],
+  dts: false,
   sourcemap: true,
   clean: true,
   external: ["react", "react-dom", "styled-components"]
